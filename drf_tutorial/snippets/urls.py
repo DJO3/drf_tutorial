1e14cab517cf4837d01f29af311c8e77c41d9858
DJO3 <dev@pytools.org>
2015-09-03 20:41:56 -0400

    converted snippet_list to CBV

--- drf_tutorial/snippets/urls.py
+++ drf_tutorial/snippets/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from snippets.views import snippet_list, snippet_detail
+from snippets.views import SnippetList, snippet_detail
 
 urlpatterns = [
-    url(r'^snippets/$', snippet_list),
+    url(r'^snippets/$', SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail),
 ]
 
