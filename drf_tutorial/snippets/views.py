6ca6fdf77d9330eae285293a3d525fbfd6922824
DJO3 <dev@pytools.org>
2015-09-03 21:11:33 -0400

    converted snippet_detil to CBV

--- drf_tutorial/snippets/views.py
+++ drf_tutorial/snippets/views.py
@@ -24,25 +24,28 @@ class SnippetList(APIView):
 
 
 # Lists one Snippet based on primary key
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class SnippetDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            return Http404
 
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
+    def get(self, request, pk, format=None):
+            snippet = self.get_object(pk)
+            serializer = SnippetSerializer(snippet)
+            return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = SnippetSerializer(data=request.data)
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
