40a58fc4ac6f00b4e0f3deaacf9146a5faee9d4b
DJO3 <dev@pytools.org>
2015-09-06 12:11:00 -0400

    set permissions for SnippetDetail

--- drf_tutorial/snippets/views.py
+++ drf_tutorial/snippets/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 
@@ -18,6 +19,8 @@ class SnippetList(ListCreateAPIView):
 class SnippetDetail(RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
 
 # Lists all Users, read only
