967f885517ac3aa9f79d925aa1a1ad2b71a030e1
DJO3 <dev@pytools.org>
2015-09-09 19:55:26 -0400

    browsable api up and running

--- drf_tutorial/snippets/urls.py
+++ drf_tutorial/snippets/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', api_root),
     url(r'^snippets/$', SnippetList.as_view(), name='snippet-list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', SnippetDetail.as_view(), name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', SnippetHighlight.as_view(), name='snippet-highlighter'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', SnippetHighlight.as_view(), name='snippet-highlight'),
     url(r'users/$', UserList.as_view(), name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', UserDetail.as_view(), name='user-detail')
 ]
