9ab649eed8c585f3d3efa73367ab4bf88a41c33e
DJO3 <dev@pytools.org>
2015-09-09 19:35:17 -0400

    added api endpoint views

--- drf_tutorial/snippets/views.py
+++ drf_tutorial/snippets/views.py
@@ -1,13 +1,35 @@
 from django.contrib.auth.models import User
+from rest_framework import generics, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 
+# Entry point for API
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+# Return highlighted snippet
+class SnippetHighlighter(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = renderers.StaticHTMLRenderer
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
 # Lists all Snippets
-class SnippetList(ListCreateAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
@@ -16,7 +38,7 @@ class SnippetList(ListCreateAPIView):
 
 
 # Lists one Snippet based on primary key
-class SnippetDetail(RetrieveUpdateDestroyAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
@@ -24,12 +46,12 @@ class SnippetDetail(RetrieveUpdateDestroyAPIView):
 
 
 # Lists all Users, read only
-class UserList(ListAPIView):
+class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 # List one User based on primary key, read only
-class UserDetail(RetrieveAPIView):
+class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
