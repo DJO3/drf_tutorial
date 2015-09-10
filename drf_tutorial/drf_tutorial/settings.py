ab133b743602722a4b54fef2e9940aaa208692d0
DJO3 <dev@pytools.org>
2015-09-09 20:29:50 -0400

    added pagination

--- drf_tutorial/drf_tutorial/settings.py
+++ drf_tutorial/drf_tutorial/settings.py
@@ -102,3 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django Rest Framework Pagination
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
